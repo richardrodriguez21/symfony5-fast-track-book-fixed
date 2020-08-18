81bf098c09eb4d04d6f32688e70dee17bddefd91
rrodriguez2 <rrodriguez2@werfen.com>
2020-08-18 06:10:12 +0200

    Enable Symfony Http Proxy on Dev to Debug/Profile Cache setup. Add Cache to ConferenceController Response Add a new Conference Controller to get only the list of Conferences a to be use to do Sub Requests. Add Sub Request on base.index.html.twig using Twig.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -41,7 +41,21 @@ class ConferenceController extends AbstractController {
    * @Route("/", name="homepage")
    */
   public function index(ConferenceRepository $conferenceRepository) {
-    return new Response($this->twig->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findAll(),]));
+    $response = new Response($this->twig->render('conference/index.html.twig', [
+      'conferences' => $conferenceRepository->findAll(),
+    ]));
+    $response->setSharedMaxAge(3600);
+    return $response;
+  }
+
+  /**
+   ** @Route("/conference_header", name="conference_header")
+   **/
+  public function conferenceHeader(ConferenceRepository $conferenceRepository) {
+    return new Response($this->twig->render('conference/header.html.twig',
+      [
+        'conferences' => $conferenceRepository->findAll(),
+      ]));
   }
 
   /**
