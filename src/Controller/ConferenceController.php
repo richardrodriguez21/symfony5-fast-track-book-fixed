83e3af3bea5e6c72197fcda3dbb72320d672960e
rrodriguez2 <rrodriguez2@werfen.com>
2020-09-06 10:40:15 +0200

    Add Symfony Notifier Component. Add Web notifications (using Symfony flash messages) on Comment's submit.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -18,6 +18,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -63,7 +65,8 @@ class ConferenceController extends AbstractController {
   /**
    * @Route("/conference/{slug}", name="conference")
    */
-  public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository, string $photoDir) {
+  public function show(Request $request, Conference $conference,
+                       CommentRepository $commentRepository, NotifierInterface $notifier, string $photoDir) {
 
     $comment = new Comment();
     $form = $this->createForm(CommentFormType::class, $comment);
@@ -94,7 +97,14 @@ class ConferenceController extends AbstractController {
       $this->entityManager->persist($comment);
       $this->entityManager->flush();
       $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
+
+      $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
+
       return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+    }
+
+    if ($form->isSubmitted()) {
+      $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
     }
 
     $offset = max(0, $request->query->getInt('offset', 0));
